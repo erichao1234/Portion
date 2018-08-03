72665c362fc17e29aa1fbc7adfac6bcb57854961
rhou <rhou@umich.edu>
2018-08-03 22:44:36 +0200

    TESTES

--- app/src/main/java/potatobotproductions/portion/MainActivity.java
+++ app/src/main/java/potatobotproductions/portion/MainActivity.java
@@ -2,6 +2,7 @@ package potatobotproductions.portion;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView hithere = findViewById(R.id.LUL);
+        hithere.setText("WOAH THERE!");
     }
 }
